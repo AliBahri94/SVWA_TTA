27a0f6cc91e3fa0f348b2a17797a7820bd0fa875
Ali <alibahri.72.dl.k@gamil.com>
2024-11-01 18:52:30 -0400

    update commands

--- commands.sh
+++ commands.sh
@@ -6,26 +6,16 @@ CUDA_VISIBLE_DEVICES=0 python train.py --only_cls --config cfgs/pre_train/pretra
 CUDA_VISIBLE_DEVICES=0 python train.py --only_cls --config cfgs/pre_train/pretrain_shapenetcore.yaml --dataset shapenetcore
 
 #TTT - Online
-CUDA_VISIBLE_DEVICES=0 python ttt.py --dataset_name modelnet --online --grad_steps 1 --config cfgs/tta/tta_modelnet.yaml --ckpts models/modelnet_jt.pth
+CUDA_VISIBLE_DEVICES=0 python ttt.py --dataset_name modelnet --online --config cfgs/tta/tta_modelnet.yaml --ckpts models/modelnet_src_only_PointMAE.pth
 
-CUDA_VISIBLE_DEVICES=0 python ttt.py --dataset_name scanobject --online --grad_steps 1 --config cfgs/tta/tta_scanobj.yaml --ckpts models/scanobject_jt.pth
+CUDA_VISIBLE_DEVICES=0 python ttt.py --dataset_name scanobject --online --config cfgs/tta/tta_scanobj.yaml --ckpts models/objectnn_src_only_PointMAE.pth
 
-CUDA_VISIBLE_DEVICES=0 python ttt.py --dataset_name shapenetcore --online --grad_steps 1 --config cfgs/tta/tta_shapenet.yaml --ckpts models/shapenet_jt.pth
-
-#TTT - Standard
-CUDA_VISIBLE_DEVICES=0 python ttt.py --dataset_name modelnet --grad_steps 20 --config cfgs/tta/tta_modelnet.yaml --ckpts models/modelnet_jt.pth
-
-CUDA_VISIBLE_DEVICES=0 python ttt.py --dataset_name scanobject --grad_steps 20 --config cfgs/tta/tta_scanobj.yaml --ckpts models/scanobject_jt.pth
-
-CUDA_VISIBLE_DEVICES=0 python ttt.py --dataset_name shapenetcore --grad_steps 20 --config cfgs/tta/tta_shapenet.yaml --ckpts models/shapenet_jt.pth
+CUDA_VISIBLE_DEVICES=0 python ttt.py --dataset_name shapenetcore --online --config cfgs/tta/tta_shapenet.yaml --ckpts models/shapenet_src_only_PointMAE.pth
 
 
 #Inference only
 CUDA_VISIBLE_DEVICES=0 python test.py --dataset_name modelnet --config cfgs/tta/tta_modelnet.yaml --ckpts models/modelnet_src_only.pth --test_source
-CUDA_VISIBLE_DEVICES=0 python test.py --dataset_name modelnet --config cfgs/tta/tta_modelnet.yaml --ckpts models/modelnet_jt.pth --test_source
 
 CUDA_VISIBLE_DEVICES=0 python test.py --dataset_name scanobject --config cfgs/tta/tta_scanobj.yaml --ckpts models/scanobject_src_only.pth --test_source
-CUDA_VISIBLE_DEVICES=0 python test.py --dataset_name scanobject --config cfgs/tta/tta_scanobj.yaml --ckpts models/scanobject_jt.pth --test_source
 
 CUDA_VISIBLE_DEVICES=0 python test.py --dataset_name shapenetcore --config cfgs/tta/tta_shapenet.yaml --ckpts models/shapenet_src_only.pth --test_source
-CUDA_VISIBLE_DEVICES=0 python test.py --dataset_name shapenetcore --config cfgs/tta/tta_shapenet.yaml --ckpts models/shapenet_jt.pth --test_source
